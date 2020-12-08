af6f4bd6076eda1b36fa8465c150bd41c1643b00
lakmil <lakmil1999@gmail.com>
2020-12-08 19:35:20 +0530

    Updates

---
+++ precache-manifest.94ca34190a7ee224c4847e726c4cb103.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7bdfdf2aba0a48753b0e28fa7230c7f",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "91afa3716b43d841560f",
+    "url": "/covidtracker/static/css/main.b0703ec4.chunk.css"
+  },
+  {
+    "revision": "66dcbb3c00908881f074",
+    "url": "/covidtracker/static/js/2.7d33abf6.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtracker/static/js/2.7d33abf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91afa3716b43d841560f",
+    "url": "/covidtracker/static/js/main.f25903b4.chunk.js"
+  },
+  {
+    "revision": "b1b4c3a8ece52a50a168",
+    "url": "/covidtracker/static/js/runtime-main.54ae7631.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidtracker/static/media/image.d7265326.png"
+  }
+]);
